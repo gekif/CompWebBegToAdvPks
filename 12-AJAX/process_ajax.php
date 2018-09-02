6318604ea456d7ecb35d3b35d97a000ab60e3986
Gekif <dzulfikar.maulana@gmail.com>
2018-09-02 07:01:05 +0700

    URL variables and Ajax

--- 12-AJAX/process_ajax.php
+++ 12-AJAX/process_ajax.php
@@ -3,14 +3,30 @@
 echo 'This is PHP page';
 echo '<br>';
 echo '<br>';
-print 'The Second Line';
+print '<strong>The Second Line</strong>';
 echo '<br>';
 echo '<br>';
 
-$c = 1;
 $names = ['Mark', 'John', 'Shaun', 'Lara'];
+$c = 1;
 
 foreach ($names as $name) {
-    echo $c . ' ' . $name . '<br><br>';
+
+    if ($_REQUEST['var1'] === $name) {
+        echo $c . ' ' . $name . ' is the important name<br><br>';
+    } else {
+        echo $c . ' ' . $name . '<br><br>';
+    }
+
     $c++;
+}
+if (isset($_REQUEST['var2'])) {
+    if ($_REQUEST['var2'] == '') {
+        echo 'We have an empty variable so we unable to show the result';
+    } else {
+        echo 'We have some ' . $_REQUEST['var2'] .  '. We will need to  eat them';
+    }
+} else {
+    echo 'Note: Something is more there but not visible because variable inside it
+    which in note declared';
 }
