d975334a4eb1b7efe4e133046003b2df51b7de9e
Gekif <dzulfikar.maulana@gmail.com>
2018-09-02 07:24:31 +0700

    Input and Output

--- 12-AJAX/process_ajax.php
+++ 12-AJAX/process_ajax.php
@@ -1,32 +1,20 @@
 <?php
 
-echo 'This is PHP page';
-echo '<br>';
-echo '<br>';
-print '<strong>The Second Line</strong>';
-echo '<br>';
-echo '<br>';
-
-$names = ['Mark', 'John', 'Shaun', 'Lara'];
+$countries = [
+    'Indonesia',
+    'Malaysia',
+    'Thailand',
+    'Germany'
+];
 $c = 1;
-
-foreach ($names as $name) {
-
-    if ($_REQUEST['var1'] === $name) {
-        echo $c . ' ' . $name . ' is the important name<br><br>';
-    } else {
-        echo $c . ' ' . $name . '<br><br>';
+echo '<br>';
+foreach ($countries as $country) {
+    /*    echo $c . '. ' . $country . '<br>';
+        $c++;*/
+    if (isset($_REQUEST['var1'])) {
+        if ($_REQUEST['var1'] == $country) {
+            echo '<div style="color: green;">' . $_REQUEST['var1'] . ' is in the list</div>';
+        }
     }
+}
 
-    $c++;
-}
-if (isset($_REQUEST['var2'])) {
-    if ($_REQUEST['var2'] == '') {
-        echo 'We have an empty variable so we unable to show the result';
-    } else {
-        echo 'We have some ' . $_REQUEST['var2'] .  '. We will need to  eat them';
-    }
-} else {
-    echo 'Note: Something is more there but not visible because variable inside it
-    which in note declared';
-}
