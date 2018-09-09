c88bdf1e77c71e11b35109ebf9ce5edfe99ae666
Gekif <dzulfikar.maulana@gmail.com>
2018-09-09 21:29:31 +0700

    HTTP

---
+++ 14-AngularJs/person.php
@@ -0,0 +1,22 @@
+{
+    "people": [
+        {
+            "Name": "Erik",
+            "Age": "21",
+            "City": "London",
+            "Country": "United Kingdom"
+        },
+        {
+            "Name": "Johnson",
+            "Age": "24",
+            "City": "New York",
+            "Country": "United States"
+        },
+        {
+            "Name": "Uno",
+            "Age": "31",
+            "City": "Jakarta",
+            "Country": "Indonesia"
+        }
+    ]
+}
