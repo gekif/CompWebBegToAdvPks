3358c1234a6ebc66b76af03e44aed611de9f8804
Gekif <dzulfikar.maulana@gmail.com>
2018-09-09 07:36:44 +0700

    Adding if else and error 404

--- 13-NodeJs/http.js
+++ 13-NodeJs/http.js
@@ -7,13 +7,24 @@
     util
 */
 
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+
+function response_404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Sorry, your page is not available');
+    response.end();
+}
 
 function onRequest(request, response) {
     console.log('We got request from client');
-    response.writeHead(200, {'Content-Type' : 'text/html'});
-    fs.createReadStream('./index.html').pipe(response);
+
+    if (request.method === 'GET' && request.url === '/') {
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream('./index.html').pipe(response);
+    } else {
+        response_404(response);
+    }
 }
 
 http.createServer(onRequest).listen(8081);
